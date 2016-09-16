6cea02895075bfe94488cb147de5ffdebcc6653b
QuaereVeritatem <robert.lee.martin@gmail.com>
2016-09-16 14:08:08 -0500

    implemented NSCoding and NSObject protocols
    implemented coding key structure, implemented NSCoding and NSObject protocols, implementing saving and loading meal lists. Ensured meal lists load at proper time

--- FoodTracker/FoodTracker/Meal.swift
+++ FoodTracker/FoodTracker/Meal.swift
@@ -8,8 +8,24 @@
 
 import UIKit
 
-class Meal {
+class Meal: NSObject, NSCoding {
     // MARK: Properties
+    
+    // MARK: Archiving Paths
+    
+    //a persistent path on the file system where data will be saved and loaded..
+    //access the path using the syntax Meal.ArchiveURL.path!(when accessed outside the class)
+    static let DocumentsDirectory = NSFileManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.URLByAppendingPathComponent("meals")
+    //constants marked with static keyword apply to the class instead of an instance of the class.
+    
+    // MARK: Types
+    
+    struct PropertyKey {
+        static let nameKey = "name"
+        static let photoKey = "photo"
+        static let ratingKey = "rating"
+    }
     
     var name: String
     var photo: UIImage?
@@ -23,6 +39,8 @@ class Meal {
         self.photo = photo
         self.rating = rating
         
+        super.init()
+        
         
         // Initialization should fail if there is no name or if the rating is negative.
         if name.isEmpty || rating < 0 {
@@ -30,5 +48,21 @@ class Meal {
         }
     }
     
+    // MARK: NSCoding
     
+    //The encodeWithCoder(_:) method prepares the class’s information to be archived, and the initializer unarchives the data when the class is created.
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(name, forKey: PropertyKey.nameKey)
+        aCoder.encodeObject(photo, forKey: PropertyKey.photoKey)
+        aCoder.encodeInteger(rating, forKey: PropertyKey.ratingKey)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        let name = aDecoder.decodeObjectForKey(PropertyKey.nameKey) as! String
+        // Because photo is an optional property of Meal, use conditional cast.
+        let photo = aDecoder.decodeObjectForKey(PropertyKey.photoKey) as? UIImage
+        let rating = aDecoder.decodeIntegerForKey(PropertyKey.ratingKey)
+        // Must call designated initializer.
+        self.init(name: name, photo: photo, rating: rating)
+    }
 }
